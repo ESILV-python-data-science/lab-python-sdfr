bdc98a9a47349dbebdf6d95a7e0c7bc6ac30bbfa
sdfr <dufour.sarah@hotmail.fr>
2017-12-31 17:07:55 +0100

    TP_classification

--- TP3/TP3_answers.md
+++ TP3/TP3_answers.md
@@ -1,11 +1,7 @@
-Testing the k-NN with different values of k
-> k=1: Score = 0.920916666667
-> k=2: Score = 0.91225
-> k=3: Score = 0.924416666667
-> k=4: Score = 0.927416666667
-> k=5: Score = 0.927666666667
-> k=6: Score = 0.933333333333
-> k=7: Score = 0.931833333333
+Select the is best value for k according to the accuracy on the dev set. Report the performance performance of the classifier on the test set for this value of k.
+> The best value of k on the valid set is 3: Accuracy score on the VALID set = 0.92775 / Accuracy score on the TEST set = 0.924666666667
 
-The best value is 6.
-> Test set/k=6: Score = 0.92525
+Train a Logistic Regression classifier on 80% and test on 20% of the samples. Report the accuracy and compare to the best result of the kNN classifier.
+> The best result of the kNN classifier (0.924666666667) is better than the result of the logistic regression classifier (0.859583333333)
+
+
--- TP3/classify_images.py
+++ TP3/classify_images.py
@@ -19,8 +19,10 @@ from sklearn import svm, metrics, neighbors
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Setup logging
@@ -48,6 +50,26 @@ def extract_features_subresolution(img,img_feature_size = (8, 8)):
     # return the values of the reduced image as features
     return [255 - i for i in reduced_img.getdata()]
 
+def train_and_test_classifier(clf,x_train, x_test, y_train, y_test):
+    # Do Training@
+    t0 = time.time()
+    clf.fit(x_train, y_train)
+    logger.info("Training  done in %0.3fs" % (time.time() - t0))
+
+    # Do testing
+    logger.info("Testing Classifier")
+    t0 = time.time()
+    predicted = clf.predict(x_test)
+
+    # Print score produced by metrics.classification_report and return accuracy scores
+    logger.info("Testing  done in %0.3fs" % (time.time() - t0))
+    print(metrics.classification_report(y_test,predicted))
+    train_accuracy_score = clf.score(x_train,y_train)
+    test_accuracy_score = metrics.accuracy_score(y_test,predicted)
+
+    return train_accuracy_score, test_accuracy_score
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract features, train a classifier on images and test the classifier')
     input_group = parser.add_mutually_exclusive_group(required=True)
@@ -57,7 +79,12 @@ if __name__ == "__main__":
     parser.add_argument('--limit-samples',type=int, help='limit the number of samples to consider for training')
     classifier_group = parser.add_mutually_exclusive_group(required=True)
     classifier_group.add_argument('--nearest-neighbors',type=int)
+    classifier_group.add_argument('--kNN',action='store_true', help='find best k for nearest_neighbors classifier')
+    classifier_group.add_argument('--logistic-regression',action='store_true')
     classifier_group.add_argument('--features-only', action='store_true', help='only extract features, do not train classifiers')
+    curve_group = parser.add_mutually_exclusive_group(required=False)
+    curve_group.add_argument('--learning-curve',action='store_true')
+    curve_group.add_argument('--testing-curve',action='store_true')
     args = parser.parse_args()
 
 
@@ -69,8 +96,6 @@ if __name__ == "__main__":
         # print(df_input)
 
     else:
-
-
         # Load the image list from CSV file using pd.read_csv
         # see the doc for the option since there is no header ;
         # specify the column names :  filename , class
@@ -114,6 +139,11 @@ if __name__ == "__main__":
         df_features.to_pickle(args.save_features)
         #logger.info('Saved {} features and class to {}'.format(df_features.shape,args.save_features))
 
+    if args.limit_samples:
+        df_input = df_input.sample(n=args.limit_samples)
+        y = df_input['class']
+        X = df_input.drop('class', axis = 1)
+
 
     if args.features_only:
         logger.info('No classifier to train, exit')
@@ -122,43 +152,192 @@ if __name__ == "__main__":
     # Train classifier
     logger.info("Training Classifier")
 
-    # Use train_test_split to create train/test split
-    X_train, X_test_valid, y_train, y_test_valid = train_test_split(X, y, train_size=0.6)
-    X_test, X_valid, y_test, y_valid = train_test_split(X_test_valid, y_test_valid, test_size=0.5)
+
+    if args.kNN:
+        # Use train_test_split to create train/test/valid split
+        X_train, X_test_valid, y_train, y_test_valid = train_test_split(X, y, train_size=0.6)
+        X_test, X_valid, y_test, y_valid = train_test_split(X_test_valid, y_test_valid, test_size=0.5)
+        logger.info("Train set size is {}".format(X_train.shape))
+        logger.info("Validation set size is {}".format(X_valid.shape))
+        logger.info("Test set size is {}".format(X_test.shape))
+
+        best_score = [1,0]
+
+        # Train a kNN classifier with different values of k and report the train/valid/test accuracy.
+        # Select the best value for k according to the accuracy on the dev set.
+
+        for k in range(1,5):
+            logger.info('Use kNN classifier with k= {}'.format(k))
+            clf = neighbors.KNeighborsClassifier(n_neighbors=k)
+            
+            # Do Training
+            t0 = time.time()
+            clf.fit(X_train, y_train)
+            logger.info("Training  done in %0.3fs" % (time.time() - t0))
+
+            # Do testing
+            logger.info("Testing Classifier")
+            t0 = time.time()
+            predicted = clf.predict(X_valid)
+
+            # Print score produced by metrics.classification_report and metrics.accuracy_score
+            logger.info("Testing  done in %0.3fs" % (time.time() - t0))
+            score = accuracy_score(y_valid, predicted)
+            print("Accuracy score = ", score)
+
+            if score > best_score[1]:
+                best_score = [k, score]
+
+        # Report the performance of the classifier on the test set for the best value of k
+        print()
+        print("Best accracy score on the VALID set = ",  best_score[1], " / k = ", best_score[0])
+
+        # Do testing and report he performance of the classifier for the best value of k based on the test set
+        t0 = time.time()
+        predicted = clf.predict(X_test)
+        logger.info("Testing  done in %0.3fs" % (time.time() - t0))
+        test_score = accuracy_score(y_test, predicted)
+        print()
+        print("Accuracy score on the TEST set  = ", test_score, " / k = ", best_score[0])
+        sys.exit()
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y, train_size=0.8)
     logger.info("Train set size is {}".format(X_train.shape))
-    logger.info("Validation set size is {}".format(X_valid.shape))
     logger.info("Test set size is {}".format(X_test.shape))
 
     if args.nearest_neighbors:
         # create KNN classifier with args.nearest_neighbors as a parameter
         logger.info('Use kNN classifier with k= {}'.format(args.nearest_neighbors))
         clf = KNeighborsClassifier(n_neighbors=args.nearest_neighbors)
-    else:
-        logger.error('No classifier specified')
-        sys.exit()
 
-    # Do Training@
-    t0 = time.time()
-    clf.fit(X_train, y_train)
-    logger.info("Training  done in %0.3fs" % (time.time() - t0))
+        # Do Training
+        t0 = time.time()
+        clf.fit(X_train, Y_train)
+        logger.info("Training  done in %0.3fs" % (time.time() - t0))
 
-    # Do testing
-    logger.info("Testing Classifier")
-    t0 = time.time()
-    predicted = clf.predict(X_valid)
+        # Do testing
+        logger.info("Testing Classifier")
+        t0 = time.time()
+        predicted = clf.predict(X_test)
 
-    # Print score produced by metrics.classification_report and metrics.accuracy_score
-    logger.info("Testing  done in %0.3fs" % (time.time() - t0))
-    print(accuracy_score(y_valid, predicted))
+        # Print score produced by metrics.classification_report and metrics.accuracy_score
+        logger.info("Testing  done in %0.3fs" % (time.time() - t0))
+        print(accuracy_score(Y_test, predicted))
 
-    # After testing different values of k, we find that the best value is 6. Let's print the score on the test set
 
-    # Do testing
-    logger.info("Testing Classifier")
-    t0 = time.time()
-    predicted = clf.predict(X_test)
+    if args.logistic_regression:
+        # create logistic regression classifier
+        logger.info('Use logistic regression classifier')
+        clf = LogisticRegression()
+            
+        # Do Training@
+        t0 = time.time()
+        clf.fit(X_train, Y_train)
+        logger.info("Training  done in %0.3fs" % (time.time() - t0))
 
-    # Print score produced by metrics.classification_report and metrics.accuracy_score
-    logger.info("Testing  done in %0.3fs" % (time.time() - t0))
-    print(accuracy_score(y_test, predicted))
-    
+        # Do testing
+        logger.info("Testing Classifier")
+        t0 = time.time()
+        predicted = clf.predict(X_test)
+
+        # Print score produced by metrics.classification_report and metrics.accuracy_score
+        logger.info("Testing  done in %0.3fs" % (time.time() - t0))
+        score = accuracy_score(Y_test, predicted)
+        print("Accuracy score = ", score)
+
+
+    if args.learning_curve:
+        # Study the impact of a growing training set on the accuracy
+        # Report the training and test set accuracies for the 1NN, 2NN, kNN
+        # (k being the best value for k previously found) and the Logisitic Regression.
+        growing_training_set = [0.01,0.1,0.2,0.4,0.6,0.8,1.0]
+        train_length=len(growing_training_set)
+        train_sets=np.zeros(train_length)
+        lr_accuracy_scores = np.zeros((train_length,2))
+        onenn_accuracy_scores = np.zeros((train_length,2))
+        twonn_accuracy_scores = np.zeros((train_length,2))
+        knn_accuracy_scores = np.zeros((train_length,2))
+
+        lr = LogisticRegression()
+        onenn = neighbors.KNeighborsClassifier(n_neighbors=1)
+        twonn = neighbors.KNeighborsClassifier(n_neighbors=2)
+        knn = neighbors.KNeighborsClassifier(n_neighbors=3)
+
+        for i in range(train_length):
+            X_train_bis, X_test_bis, Y_train_bis, Y_test_bis = train_test_split(X_train,Y_train, train_size=growing_training_set[i], shuffle=False)
+            logger.info("Train set size is {}% of original training set".format(growing_training_set[i]*100))
+            train_sets[i]=len(Y_train_bis)
+
+            # Logistic regression
+            train_accuracy_score, test_accuracy_score = train_and_test_classifier(lr,X_train_bis,X_test,Y_train_bis,Y_test)
+            lr_accuracy_scores[i][0]=train_accuracy_score
+            lr_accuracy_scores[i][1]=test_accuracy_score
+
+            # 1NN
+            train_accuracy_score, test_accuracy_score = train_and_test_classifier(onenn,X_train_bis,X_test,Y_train_bis,Y_test)
+            onenn_accuracy_scores[i][0]=train_accuracy_score
+            onenn_accuracy_scores[i][1]=test_accuracy_score
+
+            # 2NN
+            train_accuracy_score, test_accuracy_score = train_and_test_classifier(twonn,X_train_bis,X_test,Y_train_bis,Y_test)
+            twonn_accuracy_scores[i][0]=train_accuracy_score
+            twonn_accuracy_scores[i][1]=test_accuracy_score
+
+            # kNN
+            train_accuracy_score, test_accuracy_score = train_and_test_classifier(knn,X_train_bis,X_test,Y_train_bis,Y_test)
+            knn_accuracy_scores[i][0]=train_accuracy_score
+            knn_accuracy_scores[i][1]=test_accuracy_score
+
+        # Plot the training curves
+        plt.figure()
+        plt.title('1NN')
+        plt.plot(train_sets, onenn_accuracy_scores)
+        plt.figure()
+        plt.title('2NN')
+        plt.plot(train_sets, twonn_accuracy_scores)
+        plt.figure()
+        plt.title('3NN (best k)')
+        plt.plot(train_sets, knn_accuracy_scores)
+        plt.figure()
+        plt.title('Logistic Regression')
+        plt.plot(train_sets, lr_accuracy_scores)
+        plt.show()
+
+    if args.testing_curve:
+        # Study the impact of a growing testing set on the accuracy
+        growing_testing_set = [0.01,0.10,0.20,0.40,0.60,0.80,0.99]
+        test_length=len(growing_testing_set)
+        test_sets=np.zeros(test_length)
+        mean_accuracy = np.zeros(test_length)
+        std_accuracy = np.zeros(test_length)
+
+        lr = LogisticRegression()
+        knn = neighbors.KNeighborsClassifier(3)
+
+        for i in range(test_length):
+            test_accuracy = np.zeros(10)
+            for j in range(10):
+                X_train_bis, X_test_bis, Y_train_bis, Y_test_bis = train_test_split(X_test,Y_test, test_size=growing_testing_set[i])
+                logger.info("Test set size is {}% of original testing set".format(growing_testing_set[i]*100))
+                test_sets[i]=len(Y_test_bis)
+
+                #train_accuracy_score, test_accuracy_score = train_and_test_classifier(lr,X_train,X_test_bis,Y_train,Y_test_bis)
+                #test_accuracy[j]=test_accuracy_score
+
+                train_accuracy_score, test_accuracy_score = train_and_test_classifier(knn,X_train,X_test_bis,Y_train,Y_test_bis)
+                test_accuracy[j]=test_accuracy_score
+            mean_accuracy[i]=np.mean(test_accuracy)
+            std_accuracy[i]=np.std(test_accuracy)
+            print(test_accuracy)
+
+
+        print(mean_accuracy)
+        print(std_accuracy)
+
+        # Plot the testing curves (mean accuracy) on a plot with error bars (standard deviation of the accuracy)
+        plt.figure()	
+        plt.title('3NN')
+        plt.errorbar(test_sets, mean_accuracy, yerr=std_accuracy,fmt='o')
+        plt.show()
+
+        
